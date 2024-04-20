da240a38f8d6c6c98b5f40cc4e5e829fd7cdef32
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-04-20 16:45:27 +0100

    Add container

---
+++ src/components/ui/container.tsx
@@ -0,0 +1,11 @@
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div className="mx-auto w-full max-w-6xl px-4 xl:px-0">{children}</div>
+  );
+};
+
+export default Container;
