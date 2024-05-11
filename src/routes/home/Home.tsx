5a2ceea61ff9b7409518e34cb63d7288fe12ec1e
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-05-11 15:17:49 +0100

    Update all images to scale

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -7,32 +7,32 @@ import { MAIN, WELCOME, SERVICES } from "@/data/mainPage";
 const Home = () => {
   return (
     <>
-      <div className="mobile-home-section-bg-img tablet-home-section-bg-img ">
+      <div className="home-section-bg-img">
         <Container>
           <div className="pt-28 pb-4 grid justify-items-stretch h-screen font-italiana">
             <div className="text-start">
-              <p className="text-4xl sm:text-5xl pb-4 text-cyan-500">
+              <p className="text-4xl md:text-5xl pb-4 text-cyan-500">
                 {MAIN.topRowFirstTextLine}
               </p>
-              <p className="text-7xl sm:text-7xl text-fuchsia-500">
+              <p className="text-7xl md:text-7xl text-fuchsia-500">
                 {MAIN.topRowSecondTextLine}
               </p>
             </div>
-            <div className="text-end sm:justify-self-center my-auto">
-              <p className="text-5xl sm:text-7xl pb-4">
+            <div className="text-end md:justify-self-center my-auto">
+              <p className="text-5xl md:text-7xl pb-4">
                 {MAIN.middleRowFirstTextLine}
               </p>
-              <p className="text-5xl sm:text-7xl">
+              <p className="text-5xl md:text-7xl">
                 {MAIN.middleRowSecondTextLine}
               </p>
             </div>
-            <div className="text-start mt-auto sm:text-end text-7xl text-cyan-500">
+            <div className="text-start mt-auto md:text-end text-7xl text-cyan-500">
               {MAIN.bottomRowTextLine}
             </div>
           </div>
         </Container>
       </div>
-      <div className="mobile-home-section-welcome-bg-img pt-28">
+      <div className="home-section-welcome-bg-img pt-28">
         <Container>
           <div className="max-w-lg">
             <div className="text-4xl pb-10">
@@ -43,33 +43,37 @@ const Home = () => {
                 {WELCOME.topRowTitleSecondLine}
               </p>
             </div>
-            <p className="text-gray-700 pr-10">{WELCOME.description}</p>
+            <p className="text-gray-700 pr-10 md:text-lg">
+              {WELCOME.description}
+            </p>
           </div>
         </Container>
       </div>
-      <div className="mobile-home-section-services-bg-img pt-28 mb-28">
+      <div className="home-section-services-bg-img pt-28 mb-28">
         <Container>
-          <div className="text-4xl pb-10 text-right ">
-            <p className="text-fuchsia-500 font-italiana">{SERVICES.title}</p>
-          </div>
-          <div className="space-y-8">
-            {SERVICES.services.map((service, index) => (
-              <MainServicesContainer
-                key={`${service.title}-${index}`}
-                title={service.title}
-                titleColour={service.titleColour}
-                description={service.description}
-              >
-                <OutlineBtnWithIcon
-                  additionalStyles={service.buttonAdditionalStyles}
-                  text={service.buttonText}
-                />
-              </MainServicesContainer>
-            ))}
+          <div className="max-w-lg ml-auto md:pr-16">
+            <div className="text-4xl pb-10 text-right ">
+              <p className="text-fuchsia-500 font-italiana">{SERVICES.title}</p>
+            </div>
+            <div className="space-y-8">
+              {SERVICES.services.map((service, index) => (
+                <MainServicesContainer
+                  key={`${service.title}-${index}`}
+                  title={service.title}
+                  titleColour={service.titleColour}
+                  description={service.description}
+                >
+                  <OutlineBtnWithIcon
+                    additionalStyles={service.buttonAdditionalStyles}
+                    text={service.buttonText}
+                  />
+                </MainServicesContainer>
+              ))}
+            </div>
           </div>
         </Container>
       </div>
-      <div className="mobile-home-section-footer-bg-img pt-28 h-60"></div>
+      <div className="home-section-footer-bg-img pt-28 h-60 md:h-72"></div>
     </>
   );
 };
