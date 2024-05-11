d55de711601f68918cadd13f1544ae6a7f55539c
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-05-11 13:31:28 +0100

    Change tablet nav links color

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -3,13 +3,9 @@ import { Link } from "react-router-dom";
 
 const NavLinks = () => {
   return (
-    <nav className="space-x-4 hidden md:block w-full text-right">
+    <nav className="space-x-4 hidden md:block w-full text-right text-cyan-400">
       {routes.map((route, index) => (
-        <Link
-          key={`nav-link-${index}`}
-          to={route.href}
-          className="text-sm font-medium transition-colors"
-        >
+        <Link key={`nav-link-${index}`} to={route.href} className="font-medium">
           {route.label}
         </Link>
       ))}
