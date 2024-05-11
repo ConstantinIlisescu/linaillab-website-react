71f908b9986a5b784aa1d574d0e995b099e36870
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-05-11 10:52:56 +0100

    Move main page text to the data file

---
+++ src/data/mainPage.ts
@@ -0,0 +1,55 @@
+export const MAIN = {
+  topRowFirstTextLine: "Your",
+  topRowSecondTextLine: "Escape",
+  middleRowFirstTextLine: "From",
+  middleRowSecondTextLine: "Everyday",
+  bottomRowTextLine: "Routine",
+};
+
+export const WELCOME = {
+  topRowTitleFirstLine: "WELCOME TO",
+  topRowTitleSecondLine: "L.I Nail Lab",
+  description:
+    "L.I Nail Lab is a home-based nail salon located in the Hednesford neighbourhood, offering a unique and personalized experience that combines style and exceptional service. My cosy and inviting salon features top-of-the-line products, exclusive polish brands and colours, a range of free soft drinks and homemade coffee. My commitment to quality ensures that every visit to my salon feels like a luxurious and indulgent escape in the comfort of your own neighbourhood.",
+};
+
+export const SERVICES = {
+  title: "Services",
+  services: [
+    {
+      title: "Manicure",
+      titleColour: "text-indigo-300",
+      description: "Indulge in a classic, gel or polish manicure",
+      buttonText: "Read More",
+      buttonAdditionalStyles: "text-indigo-300 border-indigo-300",
+    },
+    {
+      title: "Pedicure",
+      titleColour: "text-cyan-400",
+      description: "Pamper your toenails with a luxurious pedicure",
+      buttonText: "Read More",
+      buttonAdditionalStyles: "text-cyan-400 border-cyan-400",
+    },
+    {
+      title: "KART",
+      titleColour: "text-emerald-400",
+      description: "Indulge in a KART spa pedicure",
+      buttonText: "Read More",
+      buttonAdditionalStyles: "text-emerald-400 border-emerald-400",
+    },
+    {
+      title: "Fungal Cleaning",
+      titleColour: "text-indigo-300",
+      description: "Fungal fix: Get your toes tappin' again",
+      buttonText: "Read More",
+      buttonAdditionalStyles: "text-indigo-300 border-indigo-300",
+    },
+    {
+      title: "Nail Correction",
+      titleColour: "text-cyan-400",
+      description: "Ingrown woes? Find lasting relief ",
+      buttonText: "Read More",
+      buttonAdditionalStyles: "text-cyan-400 border-cyan-400",
+    },
+  ],
+};
