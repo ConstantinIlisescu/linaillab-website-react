73ee343b9d91500bfc3da49993c0c582f5ebe1d0
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-05-23 20:48:25 +0100

    add images

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -1,10 +1,13 @@
 interface ContainerProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const Container: React.FC<ContainerProps> = ({ children }) => {
+const Container: React.FC<ContainerProps> = ({ children, className }) => {
   return (
-    <div className="mx-auto w-full max-w-6xl px-4 xl:px-0">{children}</div>
+    <div className={`mx-auto w-full max-w-6xl px-4 xl:px-0 ${className}`}>
+      {children}
+    </div>
   );
 };
 
--- src/routes/my-work/MyWork.tsx
+++ src/routes/my-work/MyWork.tsx
@@ -1,5 +1,28 @@
+import Container from "@/components/ui/container";
+import { imageLinksNumber } from "@/data/myWorkPage";
+
 const MyWork = () => {
-  return <div>MyWork</div>;
+  return (
+    <Container className=" pt-24">
+      <div className="grid grid-cols-2 gap-2">
+        {(() => {
+          const options = [];
+
+          for (let i = 0; i <= imageLinksNumber; i++) {
+            options.push(
+              <img
+                width="300px"
+                height="100%"
+                src={`https://storage.cloud.google.com/linaillab-my-work-images/${i}-.jpg`}
+              />
+            );
+          }
+
+          return options;
+        })()}
+      </div>
+    </Container>
+  );
 };
 
 export default MyWork;
